998f602495cc12e039bbeb82aa249a0e02f589bf
Sushiy <julivizi@yahoo.de>
2016-11-05 21:45:30 +0100

    more interactiondecorators test

---
+++ Assets/Scripts/InteractionToggleConnectDecorator.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UniRx;
+
+
+public class InteractionToggleConnectDecorator : MonoBehaviour, IInteractionDecorator
+{
+    public float MAX_VALUE = 1f;
+    public float MIN_VALUE = 0f;
+
+    public HWaypoint WaypointToToggle;
+
+    void Start()
+    {
+    }
+
+    public void OnValueChange(float alpha)
+    {
+        if(alpha >= MAX_VALUE)
+        {
+            if (WaypointToToggle)
+                WaypointToToggle.Connected = !WaypointToToggle.Connected;
+        }
+        else if (alpha == MIN_VALUE)
+        {
+        }
+    }
+}
