f637ae501086925c1b4f5d54f44d875f481d1aa1
Sushiy <julivizi@yahoo.de>
2016-11-05 18:44:31 +0100

    Wheel almost

---
+++ Assets/Scripts/InteractionWheel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InteractionWheel : InteractionGrabbable
+{
+    float m_fValue;
+
+    public Text m_textDebug;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        m_fValue = transform.rotation.y;
+        if (m_textDebug != null)
+            m_textDebug.text = m_fValue.ToString("F0");
+	}
+}
